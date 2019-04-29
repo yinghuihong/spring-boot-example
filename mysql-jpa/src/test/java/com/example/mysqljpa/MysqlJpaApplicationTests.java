5b2cdd6b8b1fb3b67177f1e7062fdd58d283156a
hongyinghui <123997654@qq.com>
2019-04-29 17:39:32 +0800

    mysql jpa with hibernate

--- mysql-jpa/src/test/java/com/example/mysqljpa/MysqlJpaApplicationTests.java
+++ mysql-jpa/src/test/java/com/example/mysqljpa/MysqlJpaApplicationTests.java
@@ -1,7 +1,10 @@
 package com.example.mysqljpa;
 
+import com.example.mysqljpa.entity.User;
+import com.example.mysqljpa.repository.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class MysqlJpaApplicationTests {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
-    public void contextLoads() {
+    public void testSave() {
+        User user = new User();
+        user.setName("yinghuihong");
+        user.setEmail("123997654@qq.com");
+        System.out.println("xxx " + userRepository.save(user));
+    }
+
+    @Test
+    public void testFindAll() {
+        System.out.println("xxx " + userRepository.findAll());
     }
 
 }
