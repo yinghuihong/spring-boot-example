d17c72a2e3266ab4caea2bd488c62e1ec61f4c1c
hongyinghui <123997654@qq.com>
2019-04-29 14:53:19 +0800

    init

---
+++ mysql-jpa/src/main/java/com/example/mysqljpa/MysqlJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysqljpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlJpaApplication.class, args);
+    }
+
+}
---
+++ mysql-mybatis/src/test/java/com/example/mysqlmybatis/MysqlMybatisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.mysqlmybatis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MysqlMybatisApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
